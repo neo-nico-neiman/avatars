7829765bdde9c5c61d1827c1239bc3ad2b88eabf
Nico Neiman <niconeiman@gmail.com>
2022-03-06 15:07:25 -0600

    gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {forwardRef, Suspense} from 'react';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls, Html} from '@react-three/drei';
+import AnimatedAvatar from './Components/Avatar/AnimatedAvatar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Canvas
+          camera={{position: [2, 0, 12.25], fov: 15}}
+          style={{
+            backgroundColor: '#111a21',
+            width: '100vw',
+            height: '100vh',
+          }}
+      >
+        <Tag position={[0.1, -1.4, 7]} description="Barba Roja" />
+        <ambientLight intensity={1.25}/>
+        <ambientLight intensity={0.1}/>
+        <directionalLight intensity={0.4}/>
+        <Suspense fallback={null}>
+          <AnimatedAvatar position={[0.025, -0.9, 0]}/>
+        </Suspense>
+        <OrbitControls/>
+      </Canvas>
   );
 }
 
+const Tag = forwardRef(({description, ...props}, ref) => {
+  return (
+      <Html ref={ref} className="description" center {...props}>
+        <p>{description}</p>
+      </Html>
+  )
+})
 export default App;
